4db054f8d202216de691dbac77768da735a8eb83
James Dey <jamesdey@Deytalyticss-iMac.local>
2021-05-14 11:49:31 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import datatable from './data.json';
+import logo from './deytalyticslogo.png';
+import { MDBContainer, MDBRow, MDBDataTableV5 } from 'mdbreact';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function TopSearch() {
+  return <div><MDBContainer><MDBRow><p></p></MDBRow><MDBRow><h5><img src={logo}></img>  Glossary</h5></MDBRow><MDBRow><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} searchTop searchBottom={false} /></MDBRow></MDBContainer></div>;
 }
-
-export default App;
