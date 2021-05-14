5f07234574e88d5f10a071cb749a334d085b2e9c
James Dey <jamesdey@Deytalyticss-iMac.local>
2021-05-14 15:30:29 +0100

    Added tabs for glossary & data dictionary@

--- src/App.js
+++ src/App.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import datatable from './data.json';
 import logo from './deytalyticslogo.png';
-import { MDBContainer, MDBRow, MDBDataTableV5 } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBDataTableV5, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink} from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-export default function TopSearch() {
-  return <div><MDBContainer><MDBRow><p></p></MDBRow><MDBRow><h5>&nbsp;<img src={logo}></img>  Glossary</h5></MDBRow><MDBRow><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} searchTop searchBottom={false} /></MDBRow></MDBContainer></div>;
+class App extends Component {
+  state = {
+    activeItem: '1'
+  };
+
+toggle = tab => e => {
+      if (this.state.activeItem !== tab) {
+        this.setState({
+          activeItem: tab
+        });
+      }
+    };
+
+
+  render(){
+  return <div>
+		<MDBContainer>
+	<Router>
+	  <MDBNav className="nav-tabs mt-5">
+          <MDBNavItem>
+            <MDBNavLink link to="#" active={this.state.activeItem === "1"} onClick={this.toggle("1")} role="tab" >
+              Glossary 
+            </MDBNavLink>
+          </MDBNavItem>
+          <MDBNavItem>
+            <MDBNavLink link to="#" active={this.state.activeItem === "2"} onClick={this.toggle("2")} role="tab" >
+              Data Dictionary 
+            </MDBNavLink>
+          </MDBNavItem>
+        </MDBNav>
+	</Router>
+        <MDBTabContent activeItem={this.state.activeItem} >
+          <MDBTabPane tabId="1" role="tabpanel">
+            <p className="mt-2">
+			<MDBRow><p></p></MDBRow>
+			<MDBRow><h5>&nbsp;<img src={logo}></img>  Glossary</h5></MDBRow>
+			<MDBRow><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} searchTop searchBottom={false} /></MDBRow>
+            </p>
+          </MDBTabPane>
+          <MDBTabPane tabId="2" role="tabpanel">
+            <p className="mt-2">
+              Quisquam aperiam, pariatur. Tempora, placeat ratione porro
+              voluptate odit minima. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Nihil odit magnam minima,
+              soluta doloribus reiciendis molestiae placeat unde eos
+              molestias.
+            </p>
+            <p>
+              Quisquam aperiam, pariatur. Tempora, placeat ratione porro
+              voluptate odit minima. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Nihil odit magnam minima,
+              soluta doloribus reiciendis molestiae placeat unde eos
+              molestias.
+            </p>
+          </MDBTabPane>
+        </MDBTabContent>
+		</MDBContainer>
+	</div>;
 }
+}
+
+export default App;
