dc4934fa63fd62c0cd75127f7dd686de1df6bea4
James Dey <jamesdey@Deytalyticss-iMac.local>
2021-05-14 12:09:51 +0100

    Formatted Logo position@

--- src/App.js
+++ src/App.js
@@ -4,5 +4,5 @@ import logo from './deytalyticslogo.png';
 import { MDBContainer, MDBRow, MDBDataTableV5 } from 'mdbreact';
 
 export default function TopSearch() {
-  return <div><MDBContainer><MDBRow><p></p></MDBRow><MDBRow><h5><img src={logo}></img>  Glossary</h5></MDBRow><MDBRow><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} searchTop searchBottom={false} /></MDBRow></MDBContainer></div>;
+  return <div><MDBContainer><MDBRow><p></p></MDBRow><MDBRow><h5>&nbsp;<img src={logo}></img>  Glossary</h5></MDBRow><MDBRow><MDBDataTableV5 hover entriesOptions={[5, 20, 25]} entries={5} pagesAmount={4} data={datatable} searchTop searchBottom={false} /></MDBRow></MDBContainer></div>;
 }
